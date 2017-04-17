f60350856934a5eeedfb23ce99953196f80a5bcf
merisahakyan <sahakyan_m@bk.ru>
2017-04-17 20:30:47 +0400

    [Required] User login and password

--- Market/Market/Models/User.cs
+++ Market/Market/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace Market.Models
 {
     public class User
     {
+        [Required]
         public string login { get; set; }
+        [Required]
         public string password { get; set; }
     }
 }
