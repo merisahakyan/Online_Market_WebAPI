28744ba8bcd9158be11f4c09abebb5a49735c31e
merisahakyan <sahakyan_m@bk.ru>
2017-04-19 21:45:05 +0400

    Self Hosting

---
+++ SelfHosting/SelfHosting/MySingleHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SelfHosting
+{
+    class MySingleHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("received an http message");
+            var task = new Task<HttpResponseMessage>(() =>
+            {
+                var msg = new HttpResponseMessage();
+                msg.Content = new StringContent("Hello");
+                return msg;
+            });
+            task.Start();
+            return task;
+        }
+    }
+}
---
+++ SelfHosting/SelfHosting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+
+namespace SelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:4841/");
+            var server = new HttpSelfHostServer(config,
+                new MySingleHttpMessageHandler());
+            var task = server.OpenAsync();
+            task.Wait();
+
+            Console.WriteLine("Server is up and runing");
+            Console.ReadLine();
+        }
+    }
+}
